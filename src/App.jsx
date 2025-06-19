7f3c0e917230a0f87c595b17720215b65238b017
Diego Rodrigo Cano <dsuccess2020@gmail.com>
2025-06-18 20:06:40 -0500

    infraestructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from '../src/components/Main/Main';
+import Api from "../src/utils/Api";
+import Header from '../src/components/Header/Header';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <Router>
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -29,7 +34,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+    </Router>
+  );
 }
 
 export default App
